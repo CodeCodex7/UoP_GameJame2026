24300bd5e247c5a6d23bfd7e4d42c5fc475efc8f
Stuart Andrews <stuart.andrews@smartcitti.com>
2026-06-18 20:13:06 +0100

    Removed GoapDemo

--- Assets/GoapDemo/Docs/GettingStarted/WorldKeys/Hunger.cs
+++
@@ -1,7 +0,0 @@
-using CrashKonijn.Goap.Runtime;
-
-namespace CrashKonijn.Docs.GettingStarted
-{
-    [GoapId("Hunger-2301b1b5-3508-464a-95fa-1c5c557c09e7")]
-    public class Hunger : WorldKeyBase {}
-}
